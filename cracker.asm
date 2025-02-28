e86aa8038274c448b4fd1f43b7212c3992d5b1af
Michael Movsesian <movsesian.me@phystech.edu>
2025-02-28 19:01:54 +0300

    is to add 2 vulnarabilities

--- cracker.asm
+++ cracker.asm
@@ -2,11 +2,19 @@
 .code
 org 100h
 
-Start:      
+Start:    
+        NOT_CORRECT_PASSWORD:
+        ;-------------------------------------------------------------
+        ;                       GREETING
+        ;-------------------------------------------------------------
         mov ah, 09h
         mov dx, offset Greeting
         int 21h
+        ;-------------------------------------------------------------
 
+        ;-------------------------------------------------------------
+        ;               GETTING OF THE BUFFER FROM USER
+        ;-------------------------------------------------------------
         mov ah, 3fh
         mov bx, 0000h
         mov cx, buffer_size
@@ -14,24 +22,32 @@ Start:
         mov dx, offset buffer
         int 21h
 
-        mov ah, 09h             ;printing of just entered passward
-        mov bp, offset buffer
-        add bp, 14
-        mov bx, '$'
-        mov [bp], bx
-        int 21h
+        cmp cx, ax
+        jne MISTAKE_HANDLE
 
-        mov si, offset PASSWORD
+        
+        ;-------------------------------------------------------------
+
+        ;mov ah, 09h             ;printing of just entered passward
+        ;mov bp, offset buffer
+        ;add bp, buffer_size
+        ;mov bx, '$'
+        ;mov [bp], bx
+        ;int 21h
+
+        ;STD                                            ;sets flag DF 1 which makes compering of strings in reversed order
+        mov si, offset PASSWORD ;+ buffer_size - 1      ;address of the string(password) to compare with
         mov bx, ds
         mov es, bx
-        mov di, offset buffer
-        mov cx, buffer_size-1
+        mov di, offset buffer ;+ buffer_size - 1        ;es:[di] absolute address of input buffer
+        mov cx, buffer_size                             ;___________________AVERAGE_vulnerability__________________ (have to change value on 0)
 
         repe cmpsb
 
         jne PERMISSION_DENIED
+
         mov ah, 09h
-        mov dx, offset permision_accepted_message
+        mov dx, offset permision_provided_message
         int 21h
         jmp Continue
 
@@ -39,6 +55,20 @@ Start:
         mov ah, 09h
         mov dx, offset permision_denied_message
         int 21h
+
+        jmp NOT_CORRECT_PASSWORD
+
+        
+
+        ;-------------------------------------------------------------
+        ;                       MISTAKE HANDLE
+        ;-------------------------------------------------------------
+        MISTAKE_HANDLE:
+        mov ah, 09h
+        mov dx, offset not_right_length_password
+        int 21h
+        jmp NOT_CORRECT_PASSWORD
+        ;---------------------END_OD_MISTAKE_HANDLE-------------------
 
         Continue:
 
@@ -48,10 +78,11 @@ Start:
         
 
 .data
-PASSWORD db "12345678901234$"
-Greeting db "For Admin right please confirm that you`re real human:", 0dh, 0ah, "$"
-permision_denied_message db "Permision denied$"
-permision_accepted_message db "Permision accepted$"
-buffer_size = 15
+PASSWORD db "12345678$"
+Greeting db "For Admin rights please enter 8 symbols-length password:", 0dh, 0ah, "$"
+not_right_length_password db "Please try again. Password have to be 8 symbols long" , 0dh, 0ah, "$"
+permision_denied_message  db "Permision denied" , 0dh, 0ah, "$"
+permision_provided_message db "Permision provided" , 0dh, 0ah, "$"
+buffer_size = 8
 buffer dw buffer_size dup (0)
 end Start

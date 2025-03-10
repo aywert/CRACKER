27750a604c76ec63d731d29ec6fe991943dd07f0
Michael Movsesian <movsesian.me@phystech.edu>
2025-03-11 00:38:36 +0300

    added another vulnerability

--- cracker.asm
+++ cracker.asm
@@ -2,8 +2,7 @@
 .code
 org 100h
 
-Start:    
-        NOT_CORRECT_PASSWORD:
+Start:   
         ;-------------------------------------------------------------
         ;                       GREETING
         ;-------------------------------------------------------------
@@ -13,7 +12,7 @@ Start:
         ;-------------------------------------------------------------
 
         ;-------------------------------------------------------------
-        ;               GETTING OF THE BUFFER FROM USER
+        ;               GETTING BUFFER FROM USER
         ;-------------------------------------------------------------
         mov di, offset buffer
         mov cx, -1
@@ -30,7 +29,7 @@ Start:
 
         jne READ_SIMBOL
         ;-------------------------------------------------------------
-        ;               END_GETTING OF THE BUFFER FROM USER
+        ;               END_GETTING BUFFER FROM USER
         ;-------------------------------------------------------------
 
         cmp cx, 0
@@ -38,7 +37,7 @@ Start:
         jmp Continue
 
         CHECK_NUM_OF_SYMBOLS:
-        cmp cx, buffer_size
+        ;cmp cx, buffer_size
         
         ;mov ah, 3fh
         ;mov bx, 0000h
@@ -48,7 +47,17 @@ Start:
         ;int 21h
 
         ;cmp cx, ax
-        
+
+        push di 
+        push bx
+        call cash_counter
+        pop bx
+        pop di
+
+        mov ax, hash
+        cmp ax, hash_const
+        je Continue
+
         jb MISTAKE_HANDLE
 
         mov read_symbols, cx
@@ -85,30 +94,46 @@ Start:
         mov dx, offset permision_denied_message
         int 21h
 
-        jmp NOT_CORRECT_PASSWORD
+        jmp EXIT
 
         
 
         ;-------------------------------------------------------------
         ;                       MISTAKE HANDLE
         ;-------------------------------------------------------------
-        MISTAKE_HANDLE:
+MISTAKE_HANDLE:
         mov ah, 09h
         mov dx, offset not_right_length_password
         int 21h
-        jmp NOT_CORRECT_PASSWORD
+        jmp EXIT
         ;---------------------END_OD_MISTAKE_HANDLE-------------------
 
-        Continue:
+Continue:
         mov ah, 09h
         mov dx, offset victory_message
         int 21h
 
+EXIT:
         mov ax, 4c00h
         int 21h
 
+;---------------------------------------------------------------------
+;Entry:  CX - how many to count 
+;Exit: 
+;Destr: AX DI Cash BX
+;---------------------------------------------------------------------
+        cash_counter proc
+        xor bx, bx
+        mov di, offset buffer
+        CashCount:
+        mov bl, ds:[di]
+        inc di
+        add hash, bx
+        loop CashCount
         
-
+        ret
+        endp
+        
 .data
 
 Greeting db "For Admin rights please enter 8 symbols-length password:", 0dh, 0ah, "$"
@@ -118,7 +143,11 @@ permision_denied_message  db "Permision denied" , 0dh, 0ah, "$"
 permision_provided_message db "Permision provided" , 0dh, 0ah, "$"
 buffer_size = 8
 
+hash dw 0
+
+hash_const dw 0c3h
 buffer db buffer_size + 1 dup (0) ;+ 1 for a symbol serving as the ending of the string
+
 PASSWORD db "12345678"
 read_symbols dw 0
 
